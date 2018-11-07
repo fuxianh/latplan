9d984ef270a9d1159cd1cbf85b77e7f0cb081553
Masataro Asai Masataro.Asai@ibm.com <guicho2.71828@gmail.com>
2018-11-07 16:02:22 +0900

    [script]

--- train_all.sh
+++ train_all.sh
@@ -6,33 +6,29 @@ trap exit SIGINT
 
 ulimit -v 16000000000
 
-./strips.py learn_plot_dump puzzle ConvolutionalGumbelAE mandrill 3 3 36 20000
-./state_discriminator3.py samples/puzzle_mandrill_3_3_36_20000_conv/ learn_test
-./action_autoencoder.py   samples/puzzle_mandrill_3_3_36_20000_conv/ learn_test
-./action_discriminator.py samples/puzzle_mandrill_3_3_36_20000_conv/ learn_test
+dir=$(dirname $(dirname $(readlink -ef $0)))
 
-./strips.py learn_plot_dump puzzle ConvolutionalGumbelAE mnist 3 3 36 20000
-./state_discriminator3.py samples/puzzle_mnist_3_3_36_20000_conv/ learn_test
-./action_autoencoder.py   samples/puzzle_mnist_3_3_36_20000_conv/ learn_test
-./action_discriminator.py samples/puzzle_mnist_3_3_36_20000_conv/ learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./strips.py learn_plot_dump puzzle    ConvolutionalGumbelAE mandrill 3 3 36 20000
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./strips.py learn_plot_dump puzzle    ConvolutionalGumbelAE mnist 3 3 36 20000
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./strips.py learn_plot_dump puzzle    ConvolutionalGumbelAE spider 3 3 36 20000
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./strips.py learn_plot_dump lightsout ConvolutionalGumbelAE digital 4 36 20000
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./strips.py learn_plot_dump lightsout ConvolutionalGumbelAE twisted 4 36 20000
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./strips.py learn_plot_dump hanoi     ConvolutionalGumbelAE 4 3 36 81
 
-./strips.py learn_plot_dump puzzle ConvolutionalGumbelAE spider 3 3 36 20000
-./state_discriminator3.py samples/puzzle_spider_3_3_36_20000_conv/ learn_test
-./action_autoencoder.py   samples/puzzle_spider_3_3_36_20000_conv/ learn_test
-./action_discriminator.py samples/puzzle_spider_3_3_36_20000_conv/ learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./state_discriminator3.py samples/lightsout_ConvolutionalGumbelAE_digital_4_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./state_discriminator3.py samples/lightsout_ConvolutionalGumbelAE_twisted_4_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./state_discriminator3.py samples/puzzle_ConvolutionalGumbelAE_mandrill_3_3_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./state_discriminator3.py samples/puzzle_ConvolutionalGumbelAE_mnist_3_3_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./state_discriminator3.py samples/puzzle_ConvolutionalGumbelAE_spider_3_3_36_20000 learn_test
 
-./strips.py learn_plot_dump lightsout ConvolutionalGumbelAE digital 4 36 20000
-./state_discriminator3.py samples/lightsout_digital_4_36_20000_conv/ learn_test
-./action_autoencoder.py   samples/lightsout_digital_4_36_20000_conv/ learn_test
-./action_discriminator.py samples/lightsout_digital_4_36_20000_conv/ learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./action_autoencoder.py samples/lightsout_ConvolutionalGumbelAE_digital_4_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./action_autoencoder.py samples/lightsout_ConvolutionalGumbelAE_twisted_4_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./action_autoencoder.py samples/puzzle_ConvolutionalGumbelAE_mandrill_3_3_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./action_autoencoder.py samples/puzzle_ConvolutionalGumbelAE_mnist_3_3_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./action_autoencoder.py samples/puzzle_ConvolutionalGumbelAE_spider_3_3_36_20000 learn_test
 
-./strips.py learn_plot_dump lightsout ConvolutionalGumbelAE twisted 4 36 20000
-./state_discriminator3.py samples/lightsout_twisted_4_36_20000_conv/ learn_test
-./action_autoencoder.py   samples/lightsout_twisted_4_36_20000_conv/ learn_test
-./action_discriminator.py samples/lightsout_twisted_4_36_20000_conv/ learn_test
-
-./strips.py learn_plot_dump hanoi ConvolutionalGumbelAE 4 3 36 81
-# ./state_discriminator3.py samples/hanoi_4_3_36_81_conv/ learn_test
-# ./action_autoencoder.py   samples/hanoi_4_3_36_81_conv/ learn_test
-# ./action_discriminator.py samples/hanoi_4_3_36_81_conv/ learn_test
-
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./action_discriminator.py samples/lightsout_ConvolutionalGumbelAE_digital_4_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./action_discriminator.py samples/lightsout_ConvolutionalGumbelAE_twisted_4_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./action_discriminator.py samples/puzzle_ConvolutionalGumbelAE_mandrill_3_3_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./action_discriminator.py samples/puzzle_ConvolutionalGumbelAE_mnist_3_3_36_20000 learn_test
+PYTHONPATH=$dir:$PYTHONPATH PYTHONUNBUFFERED=1 ./action_discriminator.py samples/puzzle_ConvolutionalGumbelAE_spider_3_3_36_20000 learn_test
